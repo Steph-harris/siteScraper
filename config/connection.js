e479ee74f3fedf719f632c30aee9b231455bb91e
steph-harris <steph.harris21@gmail.com>
2016-04-06 13:00:03 -0400

    got db to work in production

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
+//database configuration
 require("dotenv").config();
 var mongoose = require('mongoose');
-
 var db = mongoose.connection;
-debugger;
+
 if(process.env.NODE_ENV === 'production') {
   // HEROKU DB
   console.log(process.env.MONGOLAB_URI);
@@ -24,30 +24,6 @@ db.once('open', function() {
 });
 
 module.exports = db;
-
-//database configuration
-// require("dotenv").config();
-// var mongoose = require("mongoose");
-
-// if(process.env.NODE_ENV === 'production') {
-//   // HEROKU DB
-//   console.log(process.env.MONGOLAB_URI);
-
-//   mongoose.connect(process.env.MONGOLAB_URI);
-// }
-// else {
-//   // LOCAL DB
-//   var dbURI = 'mongodb://localhost/scraped';
-//   console.log("Local mongoose connection is ", dbURI);
-//   mongoose.connect(dbURI);
-// }
-
-// mongoose.connection.on("connected", function() {
-//   console.log("mongoose connected to " + dbURI);
-
-// mongoose.connection.on("error", function() {
-//   console.log("mongoose connection err ");
-// });
 
 // mongoose.connection.on("disconnected", function() {
 //   console.log("mongoose disconnected");
@@ -71,18 +47,3 @@ module.exports = db;
 //     process.exit(0);
 //   });
 // });
-
-// SAVE THIS
-// var mongoose = require('mongoose');
-
-// mongoose.connect('mongodb://localhost/scraped');
-// var db = mongoose.connection;
-
-// db.on('error', function(err) {
-//   console.log('Mongoose Error: ', err);
-// });
-// db.once('open', function() {
-//   console.log('Mongoose connection successful.');
-// });
-
-// module.exports = db;
