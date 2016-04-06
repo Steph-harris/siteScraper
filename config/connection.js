bb63dad61af6401fbf8191839843bdcaa3f593ee
steph-harris <steph.harris21@gmail.com>
2016-04-06 01:51:42 -0400

    updated db to use mongolab

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,61 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
+// var uri = require("./.env")
+// // mongoose.connect('mongodb://localhost/scraped');
+// mongoose.connect(uri);
+// var db = mongoose.connection;
+
+// db.on('error', function(err) {
+//   console.log('Mongoose Error: ', err);
+// });
+// db.once('open', function() {
+//   console.log('Mongoose connection successful.');
+// });
+
+// module.exports = db;
 
 //database configuration
-mongoose.connect('mongodb://localhost/scraped');
-var db = mongoose.connection;
+require("dotenv").config();
+var mongoose = require("mongoose");
 
-db.on('error', function(err) {
-  console.log('Mongoose Error: ', err);
-});
-db.once('open', function() {
-  console.log('Mongoose connection successful.');
+if(process.env.NODE_ENV === 'production') {
+  // HEROKU DB
+  console.log(process.env.MONGOLAB_URI);
+
+  mongoose.connect(process.env.MONGOLAB_URI);
+}
+else {
+  // LOCAL DB
+  var dbURI = 'mongodb://localhost/scraped';
+  console.log(dbURI);
+  mongoose.connect(dbURI);
+}
+
+mongoose.connection.on("connected", function() {
+  console.log("mongoose connected to " + dbURI);
+
+mongoose.connection.on("error", function() {
+  console.log("mongoose connection err ");
 });
 
-module.exports = db;
+mongoose.connection.on("disconnected", function() {
+  console.log("mongoose disconnected");
+});
+
+gracefulShutdown = function(msg, callback) {
+  mongoose.connection.close(function() {
+    console.log("Mongoose disconnected through " + msg);
+    callback();
+  });
+};
+
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function() {
+    process.exit(0);
+  });
+});
+
+process.on('SIGTERM', function() {
+  gracefulShutdown('heroku termination', function() {
+    process.exit(0);
+  });
+});
