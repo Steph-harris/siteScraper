0da038a00a0fde6489761de7ddf9ad5fac40e19d
steph-harris <steph.harris21@gmail.com>
2017-09-18 22:54:59 -0400

    added lodash, working on table for standings

--- public/scripts/standings.js
+++ public/scripts/standings.js
@@ -10,13 +10,31 @@ $(document).ready(function(){
         var stDt = dt.standings_date;
         var teamsLn = dt.standing.length;
         var teams = dt.standing;
-        var stStr = "(As of " + stDt.slice(5, 10) + ")";
+        var newRws;
+        var i =0;
+        var stStr = "(Standings as of " + stDt.slice(5, 10) + ")";
+        var newTb = `<table id='Standings_Tbl'><thead>
+        <th>Division</th>
+        <th>Team</th>
+        <th>W</th>
+        <th>L</th>
+        <th>G.B.</th>
+        </thead></table>`;
 
-        for(var i = 0; i<teamsLn; i++){
-          console.log(teams[i].team_id);
-        }
+        _.forEach(teams, function(val){
+          if(i%5 == 0){
+            newRws += `<tr><td></td><td>${val.conference}</td><td>${val.division}</td><td></td></tr>`;
+          }
+          newRws += `<tr><td>${val.conference} ${val.division}</td><td>${val.first_name} ${val.last_name }</td><td>${val.won}</td><td>${val.lost}</td><td>${val.games_back}</td></tr>`;
+          i++;
+        });
 
+        $("#Standings").attr("title", stStr);
+        $("#Standings").append(newTb);
+        $("#Standings_Tbl").append(newRws);
         removePreloader("#Standings");
     });
   }();
 });
+//adjust length for standings
+//add more detailed league
