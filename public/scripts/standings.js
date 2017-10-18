6c3335da7f66d47e686fd0e441a02a78ebdee1c1
steph-harris <steph.harris21@gmail.com>
2017-10-17 23:54:03 -0400

    temp cors proxy

--- public/scripts/standings.js
+++ public/scripts/standings.js
@@ -3,18 +3,20 @@ $(document).ready(function(){
     preloader("#Standings");
     //do Ajax
     $.ajax({
+      headers: { "Accept": "application/json"},
       method: "GET",
-      url: "https://erikberg.com/mlb/standings.json",
+      url: "https://cors-anywhere.herokuapp.com/https://erikberg.com/mlb/standings.json",
+      crossDomain: true,
       dataType: "json",
     }).done(function(dt){
+      console.log(dt);
         var stDt = dt.standings_date;
         var teamsLn = dt.standing.length;
         var teams = dt.standing;
         var newRws;
         var i =0;
-        var stStr = "(Standings as of " + stDt.slice(5, 10) + ")";
+        var stStr = "(Standings as of " + stDt.substr(5, 5) + ")";
         var newTb = `<table id='Standings_Tbl'><thead>
-        <th>Division</th>
         <th>Team</th>
         <th>W</th>
         <th>L</th>
@@ -23,9 +25,9 @@ $(document).ready(function(){
 
         _.forEach(teams, function(val){
           if(i%5 == 0){
-            newRws += `<tr><td></td><td>${val.conference}</td><td>${val.division}</td><td></td></tr>`;
+            newRws += `<tr><td>${val.conference} ${val.division}</td><td></td><td></td></tr>`;
           }
-          newRws += `<tr><td>${val.conference} ${val.division}</td><td>${val.first_name} ${val.last_name }</td><td>${val.won}</td><td>${val.lost}</td><td>${val.games_back}</td></tr>`;
+          newRws += `<tr><<td>${val.first_name} ${val.last_name }</td><td>${val.won}</td><td>${val.lost}</td><td>${val.games_back}</td></tr>`;
           i++;
         });
 
