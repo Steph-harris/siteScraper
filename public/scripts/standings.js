8e608c1a75ad35563aa713cb894b10e97ad4c6b8
steph-harris <steph.harris21@gmail.com>
2019-04-29 19:31:40 -0400

    setting standing division order and adding new columns

--- public/scripts/standings.js
+++ public/scripts/standings.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var getMLBStandings = function(){
     preloader("#Standings");
-    //do Ajax
+
     $.ajax({
       headers: { "Accept": "application/json"},
       method: "GET",
@@ -10,18 +10,22 @@ $(document).ready(function(){
       dataType: "json",
     }).done(function(dt){
       console.log(dt);
-        var stDt = dt.standings_date;
-        var teamsLn = dt.standing.length;
-        var teams = dt.standing;
-        var newRws;
-        var i =0;
-        var stStr = "(Standings as of " + stDt.substr(5, 5) + ")";
-        var newTb = `<table id='Standings_Tbl'><thead>
-        <th>Team</th>
-        <th>W</th>
-        <th>L</th>
-        <th>G.B.</th>
-        </thead></table>`;
+      var newObj = {};
+      var stDt = dt.standings_date;
+      var teamsLn = dt.standing.length;
+      var teams = dt.standing;
+      var i = 0;
+      var stStr = "(Standings as of " + stDt.substr(5, 5) + ")";
+      var newTb = `<table id='Standings_Tbl'>
+                    <thead>
+                      <th>Team</th>
+                      <th>W</th>
+                      <th>L</th>
+                      <th>G.B.</th>
+                      <th>Last 10</th>
+                      <th>Streak</th>
+                    </thead>
+                  </table>`;
 
         _.forEach(teams, function(val){
           switch(val.division){
@@ -36,32 +40,45 @@ $(document).ready(function(){
               break;
           }
 
+          var newDiv = `${val.conference} ${division}`;
+          var rowEntry = "";
+
           if(i%5 == 0){
-            newRws += `<tr>
-                        <td class="div_header" title="${val.conference} ${division} Division Standings">
-                        <b>${val.conference} ${division}</b>
+            rowEntry += `<tr>
+                        <td class="div_header" title="${newDiv} Division Standings">
+                        <b>${newDiv}</b>
+                        </td><td>
+                        </td><td>
                         </td><td>
                         </td><td>
                         </td><td>
                         </td>
                       </tr>`;
           }
-          newRws += `<tr title="${val.first_name} ${val.last_name } Standing">
+          rowEntry += `<tr title="${val.first_name} ${val.last_name } Standing">
                       <<td>${val.first_name} ${val.last_name }</td>
                       <td>${val.won}</td>
                       <td>${val.lost}</td>
                       <td>${val.games_back}</td>
+                      <td>${val.last_ten}</td>
+                      <td>${val.streak}</td>
                     </tr>`;
+
+          newObj[newDiv] += rowEntry;
           i++;
         });
 
         $("#Standings").attr("title", stStr);
         $("#Standings").append(newTb);
-        $("#Standings_Tbl").append(newRws);
+        $("#Standings_Tbl").append(newObj['NL East']);
+        $("#Standings_Tbl").append(newObj['NL Central']);
+        $("#Standings_Tbl").append(newObj['NL West']);
+        $("#Standings_Tbl").append(newObj['AL East']);
+        $("#Standings_Tbl").append(newObj['AL Central']);
+        $("#Standings_Tbl").append(newObj['AL West']);
         $(".div_header").parent().css("background-color", "#004685");
         removePreloader("#Standings");
     });
   }();
 });
-//adjust length for standings
 //add more detailed league
