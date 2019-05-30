18576aec435de96019754e665a09eaad1762d52f
Stephanie Harris <stephanieh@aweber.com>
2019-05-29 22:06:37 -0400

    adding league select

--- public/scripts/standings.js
+++ public/scripts/standings.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+  $("#americanTbl").hide();
+
+  $("#leagueSelect input").on("click", function(){
+    $("table").toggle();
+    $("#leagueSelect input").toggleClass("activeBtn fadeBtn");
+  });
+
   var getMLBStandings = function(){
     preloader("#Standings");
 
@@ -15,38 +22,10 @@ $(document).ready(function(){
       var teamsLn = dt.standing.length;
       var teams = dt.standing;
       var i = 0;
-      var stStr = "(Standings as of " + stDt.substr(5, 5) + ")";
-      var newTb = `<div id="tableDiv">
-                      <table id='Standings_Tbl'>
-                        <thead>
-                          <th>Team</th>
-                          <th>W</th>
-                          <th>L</th>
-                          <th>G.B.</th>
-                          <th>Last 10</th>
-                          <th>Streak</th>
-                        </thead>
-                      </table>
-                  </div>`;
+      var standingsDate = "(Standings as of " + stDt.substr(5, 5) + ")";
       var nationalTb = `<table id='nationalTbl'>
-                          <thead>
-                            <th>Team</th>
-                            <th>W</th>
-                            <th>L</th>
-                            <th>G.B.</th>
-                            <th>Last 10</th>
-                            <th>Streak</th>
-                          </thead>
                         </table>`;
       var americanTb = `<table id='americanTbl'>
-                          <thead>
-                            <th>Team</th>
-                            <th>W</th>
-                            <th>L</th>
-                            <th>G.B.</th>
-                            <th>Last 10</th>
-                            <th>Streak</th>
-                          </thead>
                         </table>`;
         _.forEach(teams, function(val){
           switch(val.division){
@@ -65,19 +44,19 @@ $(document).ready(function(){
           var rowEntry = "";
 
           if(i%5 == 0){
-            rowEntry += `<tr>
+            rowEntry += `<tr class="whiteColor">
                         <td class="div_header" title="${newDiv} Division Standings">
-                        <b>${division}</b>
-                        </td><td>
-                        </td><td>
-                        </td><td>
-                        </td><td>
-                        </td><td>
+                          ${division}
                         </td>
+                        <td>W</td>
+                        <td>L</td>
+                        <td>G.B.</td>
+                        <td>Last 10</td>
+                        <td>Streak</td>
                       </tr>`;
           }
           rowEntry += `<tr title="${val.first_name} ${val.last_name } Standing">
-                        <<td>${val.first_name} ${val.last_name }</td>
+                        <td>${val.first_name} ${val.last_name }</td>
                         <td>${val.won}</td>
                         <td>${val.lost}</td>
                         <td>${val.games_back}</td>
@@ -89,14 +68,13 @@ $(document).ready(function(){
           i++;
         });
 
-        $("#Standings").attr("title", stStr);
-        $("#Standings").append(newTb);
+        $("#Standings").attr("title", standingsDate);
         $("#nationalTbl").append(newObj['NL East']);
         $("#nationalTbl").append(newObj['NL Central']);
         $("#nationalTbl").append(newObj['NL West']);
-        $("#americanTb").append(newObj['AL East']);
-        $("#americanTb").append(newObj['AL Central']);
-        $("#americanTb").append(newObj['AL West']);
+        $("#americanTbl").append(newObj['AL East']);
+        $("#americanTbl").append(newObj['AL Central']);
+        $("#americanTbl").append(newObj['AL West']);
         $(".div_header").parent().css("background-color", "#004685");
         removePreloader("#Standings");
     });
